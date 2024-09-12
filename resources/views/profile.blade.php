bc0ab495a907a351510e26a10e02d374638ffefd
FuadHmdn <fuadhamidan.drive@gmail.com>
2024-09-12 16:32:04 +0700

    Styling profile view

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,25 +5,47 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            background-color: #a3b8c8;
+        }
+
+        div {
+            text-align: center;
+        }
+
+        img {
+            border-radius: 50%;
+            width: 160px;
+            height: 160px;
+            object-fit: cover;
+        }
+
+        p {
+            background-color: #ffffff;
+            font-size: 30px;
+            margin: 13px 0;
+            padding: 10px;
+            padding-left: 60px;
+            padding-right: 60px;
+            border-radius: 8px;
+        }
+    </style>
 </head>
 
 <body>
 
-    <tr>
-        <td>Nama</td>
-        <td>:</td>
-        <td><?= $nama ?></td>
-    </tr>
-    <tr>
-        <td>Kelas</td>
-        <td>:</td>
-        <td><?= $kelas ?></td>
-    </tr>
-    <tr>
-        <td>NPM</td>
-        <td>:</td>
-        <td><?= $npm ?></td>
-    </tr>
+    <div>
+        <img src="https://media.licdn.com/dms/image/v2/D5603AQFlPkvSgGhTGw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1719026457744?e=2147483647&v=beta&t=po9_V9fcWCZALyGvmEdo8xbPD1cOmGk9GVMlAMDsiTQ" alt="Profile Picture">
+        <p>Fuad Hamidan</p>
+        <p>A</p>
+        <p>2257051025</p>
+    </div>
 
 </body>
 
