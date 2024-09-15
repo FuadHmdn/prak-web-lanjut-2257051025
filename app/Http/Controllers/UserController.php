3be5e95715189c7427566224a661427f239d71b9
FuadHmdn <fuadhamidan.drive@gmail.com>
2024-09-15 13:34:10 +0700

    add User Controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function profile($nama = "", $kelas = "", $npm =
+    "")
+    {
+        $data = [ 
+            'nama' => $nama, 
+            'kelas' => $kelas, 
+            'npm' => $npm, 
+            ];
+
+        return view('profile', $data);
+    }
 }
