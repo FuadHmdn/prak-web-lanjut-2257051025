b52a6c81e822ceedc032c1c7d04456b9eea753f7
FuadHmdn <fuadhamidan.drive@gmail.com>
2024-09-28 13:16:28 +0700

    test

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('user', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
+            $table->string('nama');
+            $table->string('npm');
+            $table->foreignId('kelas_id')->constrained();
             $table->timestamps();
         });
     }
@@ -28,6 +25,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('user');
     }
 };
+
+//////
+
