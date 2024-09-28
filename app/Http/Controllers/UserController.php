92cd08e4f985942f2ea89b07dc488766023ca0a1
FuadHmdn <fuadhamidan.drive@gmail.com>
2024-09-28 13:02:19 +0700

    add model

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Kelas;
+
 
 class UserController extends Controller
 {
-    //
+    public function create(){
+        return view('create_user', [
+            'kelas' => Kelas::all(),
+        ]);
+    }
 }
