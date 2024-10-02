6ad8141a7483f897ac103b4e558c03e0f600dfc0
FuadHmdn <fuadhamidan.drive@gmail.com>
2024-10-02 23:55:16 +0700

    add read data on list user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,26 @@ use App\Models\UserModel;
 
 class UserController extends Controller
 {
+
+    public $userModel; 
+    public $kelasModel;
+    public function __construct(){
+        $this->userModel = new UserModel(); 
+        $this->kelasModel = new Kelas(); 
+    }
+
+    public function index() 
+    { 
+        $users = $this->userModel->getUser();
+        
+        $data = [ 
+            'title' => 'Create User', 
+            'users' => $this->userModel->getUser(), 
+        ]; 
+    
+        return view('list_user', $data); 
+    } 
+
     public function profile($nama = "", $kelas = "", $npm =
     "")
     {
@@ -22,33 +42,24 @@ class UserController extends Controller
 
     public function create()
     {
-        return view('create_user', [
-            'kelas' => Kelas::all()
-        ]);
+        // $kelasModel = new Kelas();
+        $kelas = $this->kelasModel->getKelas();
+        $data = [
+            'title' => 'Create User',
+            'kelas' => $kelas,
+        ];
+
+        return view('create_user', $data);
     }
 
     public function store(UserRequest $request)
     {
-        $validateData = $request->validate([
-            'nama'=> 'required|string|max:255',
-            'npm' => 'required|string|max:255',
-            'kelas_id' => 'required|exists:kelas,id',
-        ]);
-
-        // $model = new UserModel;
-        // $model->nama = $request->nama;
-        // $model->kelas = $request->kelas;
-        // $model->npm = $request->npm;
-        // $model->save();
-
-        $user = UserModel::create($validateData);
-        $user -> load('kelas');
-        return view('profile', [
-            'nama' => $user->nama,
-            'npm' => $user->npm,
-            'nama_kelas' => $user->kelas->nama_kelas ?? 'Kelas Tidak Ditemukan'
-        ]);
-
-        // return redirect('profile');
+        $this->userModel->create([ 
+            'nama' => $request->input('nama'), 
+            'npm' => $request->input('npm'), 
+            'kelas_id' => $request->input('kelas_id'), 
+            ]); 
+        
+            return redirect()->to('/user'); 
     }
 }
