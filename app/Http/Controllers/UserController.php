c670f90387459266e15ad7cec95b23d45d673872
FuadHmdn <fuadhamidan.drive@gmail.com>
2024-10-17 16:05:33 +0700

    fie storeage uploads

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -56,54 +56,29 @@ class UserController extends Controller
 
     public function store(UserRequest $request)
     {
-        if ($request->hasFile('foto')) {
-            $foto = $request->file('foto');
-            $fotoPath = $foto->move(public_path('assets/img'), $foto->getClientOriginalName());
-        } else {
-            $fotoPath = null;
-        }
-
-        $this->userModel->create([
-            'nama' => $request->input('nama'),
-            'npm' => $request->input('npm'),
-            'kelas_id' => $request->input('kelas_id'),
-            'foto' => $fotoPath ? 'assets/img/' . $foto->getClientOriginalName() : null,
+        $request->validate([
+            'nama' => 'required',
+            'npm' => 'required',
+            'kelas_id' => 'required',
+            'foto' => 'image|file|max:2048',
         ]);
 
-        return redirect()->to('/user')->with('success', 'User berhasil ditambahkan');
+        if ($request->hasFile('foto')) {
+            $file = $request->file('foto');
+            $filename = time() . '.' . $request->foto->extension();
+
+            $file->move(public_path('uploads'), $filename);
+
+            $this->userModel->create([
+                'nama' => $request->input('nama'),
+                'npm' => $request->input('npm'),
+                'kelas_id' => $request->input('kelas_id'),
+                'foto' => 'uploads/' . $filename,
+            ]);
+        }
+
+        return redirect()->to('/user')->with('success', 'User Berhasil dibuat');
     }
-
-    // public function store(UserRequest $request)
-    // {
-    //     // Validasi input 
-    //     $request->validate([
-    //         'nama' => 'required',
-    //         'npm' => 'required',
-    //         'kelas_id' => 'required',
-    //         'foto' => 'image|file|max:2048', // Validasi foto 
-    //     ]);
-
-    //     // Proses upload foto 
-    //     if ($request->hasFile('foto')) {
-    //         $file = $request->file('foto');
-    //         $filename = time() . '_' . $file->getClientOriginalName();
-
-    //         // Simpan di folder 'uploads' di dalam 'public' 
-    //         $file->storeAs('public/uploads', $filename);
-
-    //         // Simpan data user ke database 
-    //         $this->userModel->create([
-    //             'nama' => $request->input('nama'),
-    //             'npm' => $request->input('npm'),
-    //             'kelas_id' => $request->input('kelas_id'),
-    //             'foto' => 'storage/uploads/' . $filename, // Path untuk URL publik
-    //         ]);
-    //     }
-
-    //     return redirect()->to('/user')->with('success', 'User Berhasil dibuat');
-    // }
-
-
 
     public function show($id)
     {
